9236bc630aff023d23679d0fb718803a7a711d21
YaKashubo <kashubo.work@mail.ru>
2021-11-10 13:56:39 +0300

    Homework 5 done

--- 2021.11.07-Homework5/Task2/Source.cpp
+++ 2021.11.07-Homework5/Task2/Source.cpp
@@ -7,18 +7,18 @@ int main(int argc, char* argv[])
 	double n = 0;
 	cin >> n;
 
-	long long intn = *((long long*)&n); // преобразование double в long long
+	long long intn = *((long long*)&n); 
 
 	for (int i = 0; i < sizeof(long long) * 8; i++)
 	{
 
 		unsigned long long bit = 0; 
 
-		bit = ((long long)1) << (sizeof(long long) * 8 - 1 - i); // создаём число 1000 0000 0000 ..., чтобы получить конкретный бит
+		bit = ((long long)1) << (sizeof(long long) * 8 - 1 - i); 
 
-		long long cbit = 0; // сам бит
+		long long cbit = 0;
 
-		cbit = (intn & bit) >> (sizeof(long long) * 8 - 1 - i); //рассчёт бита и сдвиг его в начало
+		cbit = (intn & bit) >> (sizeof(long long) * 8 - 1 - i); 
 		cout << cbit;
 
 		if ((i + 1) % 4 == 0)
--- 2021.11.07-Homework5/Task3/Source.cpp
+++ 2021.11.07-Homework5/Task3/Source.cpp
@@ -6,12 +6,9 @@ int main(int argc, char* argv[])
 {
 	int n = 0;
 	cin >> n;
-	
-	
 
 	int bit = 0;
 	bit = n >>(sizeof(n)*8-1)&1;
-	
 
 	int newn = 0;
 	for (int i = 0; i < sizeof(newn)*8; i++)
